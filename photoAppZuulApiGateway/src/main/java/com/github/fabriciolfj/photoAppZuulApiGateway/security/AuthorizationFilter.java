2cf4b562ca5558c3b0008bec7e7dc26c234920f8
fabricio <fabricio.jacob@outlook.com>
2020-09-07 12:30:35 -0300

    add

---
+++ photoAppZuulApiGateway/src/main/java/com/github/fabriciolfj/photoAppZuulApiGateway/security/AuthorizationFilter.java
@@ -0,0 +1,62 @@
+package com.github.fabriciolfj.photoAppZuulApiGateway.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+    private Environment environment;
+
+    public AuthorizationFilter(AuthenticationManager authenticationManager, Environment environment) {
+        super(authenticationManager);
+        this.environment = environment;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        final String authorizationHeader = request.getHeader(environment.getProperty("authorization.token.header.name"));
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith(environment.getProperty("authorization.token.header.prefix"))) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        final UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(final HttpServletRequest request) {
+        final var authorizationHeader = request.getHeader(environment.getProperty("authorization.token.header.name"));
+
+        if (authorizationHeader == null) {
+            return null;
+        }
+
+        final var token = authorizationHeader.replace(environment.getProperty("authorization.token.header.prefix"), "");
+
+        var userId = Jwts.parser()
+                .setSigningKey(environment.getProperty("token.secret"))
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        if (userId == null) {
+            return null;
+        }
+
+        return new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>());
+    }
+}
